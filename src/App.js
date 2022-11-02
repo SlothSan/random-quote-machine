14d82422dc469d234b67da05e2f236daac0a366a
Mike Oram <Mikeoram01@gmail.com>
2022-11-02 11:34:33 +0000

    Created quote-box component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import './App.css';
+import QuoteBox from "./Components/QuoteBox/QuoteBox";
 
 function App() {
+
+  const getRandomPastelColor = () => {
+      //TODO Implement this function need to setup state for this and pass to all components that need it.
+  }
+
   return (
     <div className="App">
+      <QuoteBox />
     </div>
   );
 }
---
+++ src/Components/QuoteBox/QuoteBox.js
@@ -0,0 +1,11 @@
+import './QuoteBox.css'
+
+const QuoteBox = () => {
+    return (
+        <div id={"quote-box"} className={"quote-box"}>
+
+        </div>
+    )
+}
+
+export default QuoteBox;
