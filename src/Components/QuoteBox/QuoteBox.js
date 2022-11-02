5e437cd6049f619386e04eba8b68d29cac149695
Mike Oram <Mikeoram01@gmail.com>
2022-11-02 11:44:23 +0000

    Created quote text component

--- src/Components/QuoteBox/QuoteBox.js
+++ src/Components/QuoteBox/QuoteBox.js
@@ -1,9 +1,16 @@
 import './QuoteBox.css'
+import {useState} from "react";
+import QuoteText from "./QuoteText/QuoteText";
 
 const QuoteBox = () => {
+
+    const [quoteText, setQuoteText] = useState('');
+    const [quoteAuthor, setQuoteAuthor] = useState('');
+
+
     return (
         <div id={"quote-box"} className={"quote-box"}>
-
+            <QuoteText />
         </div>
     )
 }
---
+++ src/Components/QuoteBox/QuoteText/QuoteText.js
@@ -0,0 +1,11 @@
+import './QuoteText.css'
+
+const QuoteText = () => {
+    return (
+            <p id={"text"} className={"quote-text"}>
+
+            </p>
+        )
+}
+
+export default QuoteText
